0dbf419f925dd3d29f41757403bc25ec41e5e80b
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-27 01:22:11 +0200

    init commit

---
+++ src/main/java/dev/zubeirtech/spring_jpa/SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package dev.zubeirtech.spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaApplication.class, args);
+    }
+
+}
